034a0c03567db826e2d080068267d638e2f15005
ryanrw <surasak@opn.ooo>
2023-02-14 18:23:24 +0700

    add simple example test case

---
+++ cypress/integration/Duckduckgo.js
@@ -0,0 +1,13 @@
+const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+
+When("I visit duckduckgo.com", () => {
+  cy.visit("https://www.duckduckgo.com");
+});
+
+Then("I should see a search bar", () => {
+  cy.get("input").should(
+    "have.attr",
+    "placeholder",
+    "Search the web without being tracked"
+  );
+});
